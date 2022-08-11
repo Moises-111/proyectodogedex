bdbaf09d3c5d1c8233f3a1185ca3addb52653bff
unknown <moisgp15@gmail.com>
2022-08-11 00:01:14 -0600

    se sube metodo de boton sign up

--- app/src/main/java/com/example/dogedex/auth/SingOutFragment.kt
+++ app/src/main/java/com/example/dogedex/auth/SingOutFragment.kt
@@ -1,6 +1,7 @@
 package com.example.dogedex.auth
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,13 +12,52 @@ import com.example.dogedex.databinding.FragmentSingOutBinding
 
 class SingOutFragment : Fragment() {
 
+    private lateinit var binding: FragmentSingOutBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = FragmentSingOutBinding.inflate(layoutInflater)
+        binding = FragmentSingOutBinding.inflate(layoutInflater)
+        setuoSingUpButton()
         return binding.root
+    }
+
+    private fun setuoSingUpButton() {
+        binding.signUpButton.setOnClickListener{
+            validateFields()
+        }
+    }
+
+    private fun validateFields() {
+        val email = binding.emailEdit.text.toString()
+
+        binding.emailInput.error =""
+        binding.passwordInput.error=""
+        binding.confirmPasswordInput.error=""
+
+        if(!isValidEmail(email)){
+            binding.emailInput.error = getString(R.string.email_is_not_valid)
+            return
+        }
+
+        val password = binding.passwordEdit.text.toString()
+        if(password.isEmpty()){
+            binding.passwordInput.error = getString(R.string.pass_must_not_be_empty)
+            return
+        }
+
+        val passwordConfirmation = binding.confirmPasswordEdit.text.toString()
+        if(passwordConfirmation.isEmpty()){
+            binding.confirmPasswordInput.error =getString(R.string.pass_must_not_be_empty)
+            return
+        }
+
+    }
+
+    private fun isValidEmail(email: String?) : Boolean{
+        return email.isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 
 
